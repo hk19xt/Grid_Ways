0f6a344b0c5613f134491dcffe0a5b338d8505cf
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-20 23:23:16 -0400

    Update Main.java

--- Grid_Ways/src/Grid_Ways/Main.java
+++ Grid_Ways/src/Grid_Ways/Main.java
@@ -2,6 +2,11 @@ package Grid_Ways;
 
 public class Main {
     public static void main(String[] args){
-        
+        Grid_ways gw = new Grid_ways();
+        int row = 4;
+        int column = 6;
+        int outcome = 0;
+        outcome = gw.grid_ways(row, column);
+        System.out.println(outcome);
     }
 }
